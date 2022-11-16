c76612935866a47b7bbd5bde275500fb19f1140f
shashank0711 <104528805+shashank0711@users.noreply.github.com>
2022-11-16 12:00:05 +0530

    Update utilities.dart

--- lib/utilities.dart
+++ lib/utilities.dart
@@ -1,7 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 //text form box for signup and login page for
-Widget TextformfieldSignup(controller, icon,  labeltext, bool obscure, validation){
+Widget TextformfieldSignup(
+    controller, icon, labeltext, bool obscure, validation) {
   return TextFormField(
     controller: controller,
     obscureText: obscure,
@@ -10,19 +13,19 @@ Widget TextformfieldSignup(controller, icon,  labeltext, bool obscure, validatio
       fontFamily: 'font',
       color: Colors.white,
     ),
-
     autovalidateMode: AutovalidateMode.onUserInteraction,
     validator: validation,
-
     decoration: InputDecoration(
-      prefixIcon: Icon(icon, color: Colors.white,),
+      prefixIcon: Icon(
+        icon,
+        color: Colors.white,
+      ),
       contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
       labelText: labeltext,
       labelStyle: const TextStyle(
         fontFamily: 'font',
         color: Colors.white,
       ),
-
       focusedBorder: OutlineInputBorder(
         borderRadius: BorderRadius.circular(10),
         borderSide: const BorderSide(
@@ -46,17 +49,14 @@ Widget TextformfieldSignup(controller, icon,  labeltext, bool obscure, validatio
   );
 }
 
-
-
-
-
 //ending statement of already have an acc or create a new acc
-Widget endstatement(String text1, String text2, ontap){
+Widget endstatement(String text1, String text2, ontap) {
   return Row(
     mainAxisAlignment: MainAxisAlignment.center,
     children: [
-      Text(text1,
-        style: const TextStyle(color: Colors.white,fontFamily: 'font'),
+      Text(
+        text1,
+        style: const TextStyle(color: Colors.white, fontFamily: 'font'),
       ),
       GestureDetector(
         onTap: ontap,
@@ -74,17 +74,16 @@ Widget endstatement(String text1, String text2, ontap){
   );
 }
 
-
-
-
-
 //menu of Accounts page except logout option
-Widget Accountfeatures(BuildContext context, text, page){
-
+Widget Accountfeatures(BuildContext context, text, page) {
   return TextButton(
-    onPressed: (){
-      Navigator.push(context,
-        MaterialPageRoute(builder: (context) => page,),);
+    onPressed: () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => page,
+        ),
+      );
     },
     style: TextButton.styleFrom(
       foregroundColor: Colors.grey,
@@ -95,22 +94,19 @@ Widget Accountfeatures(BuildContext context, text, page){
         Text(
           text,
           style: const TextStyle(
-              fontFamily: 'font',
-              fontSize: 15,
-              color: Colors.black
-          ),
+              fontFamily: 'font', fontSize: 15, color: Colors.black),
         ),
-        Icon(Icons.arrow_forward_ios_outlined,color: Colors.black54,),
+        const Icon(
+          Icons.arrow_forward_ios_outlined,
+          color: Colors.black54,
+        ),
       ],
     ),
   );
 }
 
-
-
-
 //text form for profile information
-Widget TextformProfile(initialvalue, labeltext){
+Widget TextformProfile(initialvalue, labeltext) {
   return TextFormField(
     // controller: controller,
     initialValue: initialvalue,
@@ -121,7 +117,6 @@ Widget TextformProfile(initialvalue, labeltext){
       fontWeight: FontWeight.bold,
     ),
 
-
     decoration: InputDecoration(
       contentPadding: EdgeInsets.fromLTRB(15, 10, 20, 10),
       labelText: labeltext,
@@ -130,7 +125,6 @@ Widget TextformProfile(initialvalue, labeltext){
         color: Colors.black54,
         fontSize: 10,
       ),
-
       focusedBorder: OutlineInputBorder(
         borderRadius: BorderRadius.circular(10),
         borderSide: const BorderSide(
@@ -152,4 +146,91 @@ Widget TextformProfile(initialvalue, labeltext){
       ),
     ),
   );
+}
+
+Future addUserDetails(
+    String firstName, String secondName, String email, int phoneNumber) async {
+  await FirebaseFirestore.instance.collection('user').add({
+    'first name': firstName,
+    'second name': secondName,
+    'email': email,
+    'phone number': phoneNumber
+  });
+}
+
+Widget Searchbar(controller, TextInputType, text, ) {
+  return Container(
+    margin: const EdgeInsets.only(left: 10, right: 10),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(25),
+          color: Colors.grey,
+      ),
+      height: 75,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: TextField(
+              cursorColor: Colors.deepOrange[300],
+              controller: controller,
+              textInputAction: TextInputAction.done,
+              keyboardType: TextInputType,
+              style: const TextStyle(
+                fontFamily: 'font',
+                color: Colors.white,
+              ),
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+                hintText: text,
+                hintStyle: const TextStyle(
+                  fontFamily: 'font',
+                  fontSize: 15,
+                  color: Colors.white,
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(25),
+                  borderSide: const BorderSide(
+                    color: Colors.grey,
+                    width: 2,
+                  ),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(25),
+                  borderSide: const BorderSide(
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+            child: TextButton(
+              onPressed: (){},
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.grey,
+              ),
+              child: Container(
+                height: 45,
+                width: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(25),
+                  color: Colors.deepOrange[300],
+                ),
+                child: const Center(
+                  child: Text(
+                    'Search',
+                    style: TextStyle(
+                      fontFamily: 'font',
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ));
 }
