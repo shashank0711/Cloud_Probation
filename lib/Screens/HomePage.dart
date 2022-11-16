bf5fca8b0d91707ba5acdd2511916c96614e14a4
shashank0711 <104528805+shashank0711@users.noreply.github.com>
2022-11-16 12:01:30 +0530

    Update HomePage.dart

--- lib/Screens/HomePage.dart
+++ lib/Screens/HomePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rail_food/utilities.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,58 +9,221 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
-  @override
+  final pnrController = TextEditingController();
+  final trainNumberController = TextEditingController();
+  final stationController = TextEditingController();
 
+  @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    TabController _tabcontroller = new TabController(length: 3, vsync: this);
+    TabController _tabcontroller = TabController(length: 3, vsync: this);
 
-    return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              //main image
-              Container(
-                height: size.height * 32 / 100,
-                child: Stack(
-                  children: [
-                    Image.asset('images/main_image.jpg'),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          body: SafeArea(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  //main image
+                  Container(
+                    height: size.height * 32 / 100,
+                    child: Stack(
                       children: [
-                        SizedBox(
-                          width: size.width * 44 / 100,
-                        ),
-                        Column(
+                        Image.asset('images/main_image.jpg'),
+                        Row(
                           mainAxisAlignment: MainAxisAlignment.center,
-                          children: const [
-                            Text(
-                              'Get your favourite\n\nfood delivered at\n\nyour train seat',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                  fontFamily: 'font',
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                  fontSize: 20),
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            SizedBox(
+                              width: size.width * 44 / 100,
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: const [
+                                Text(
+                                  'Get your favourite\n\nfood delivered at\n\nyour train seat',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      fontFamily: 'font',
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                      fontSize: 20),
+                                ),
+                              ],
                             ),
                           ],
                         ),
                       ],
                     ),
-                  ],
-                ),
+                  ),
+
+                  SizedBox(height: size.height*1/100,),
+
+                  //nav bar
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      height: 60,
+                      decoration: BoxDecoration(
+                        color: Colors.black12,
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TabBar(
+                          indicator: BoxDecoration(
+                              color: Colors.deepOrange[300],
+                              borderRadius: BorderRadius.circular(25)),
+                          labelColor: Colors.white,
+                          unselectedLabelColor: Colors.black87,
+                          tabs: const [
+                            Tab(
+                              child: Text(
+                                'PNR',
+                                style: TextStyle(
+                                  fontFamily: 'font',
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            Tab(
+                              child: Text(
+                                'TRAIN NO.',
+                                style: TextStyle(
+                                  fontFamily: 'font',
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            Tab(
+                              child: Text(
+                                'STATION',
+                                style: TextStyle(
+                                  fontFamily: 'font',
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  //search bars
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(8, 20, 8, 0),
+                    child: SizedBox(
+                      height: 80,
+                      child: TabBarView(
+                        children: [
+                          Searchbar(pnrController, TextInputType.number,
+                              "Enter PNR Number"),
+                          Searchbar(trainNumberController, TextInputType.number,
+                              "Enter Train Number"),
+                          Searchbar(stationController, TextInputType.name,
+                              "Enter Station Name"),
+                        ],
+                      ),
+                    ),
+                  ),
+                  
+
+                  SizedBox(height: size.height*4/100,),
+
+                   //text
+                   const Padding(
+                     padding: EdgeInsets.all(10.0),
+                     child: Text('Order food on train with easy steps :-',
+                  style: TextStyle(
+                        fontSize: 24,
+                      fontFamily: 'font',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      ),
+                     ),
+                   ),
+
+                  SizedBox(height: size.height* 2/100,),
+
+                  //steps
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          children: [
+                            Image.asset('images/Enter.png', scale: 2,),
+                            SizedBox(height: size.height * 1/100,),
+                            const Text('Enter Train/\nPNR number',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontFamily: 'font',
+                                fontSize: 10,
+                                fontWeight: FontWeight.bold
+                              ),
+                            ),
+                          ],
+                        ),
+
+
+                        Column(
+                          children: [
+                            Image.asset('images/Select.png', scale: 2.2,),
+                            SizedBox(height: size.height * 1/100,),
+                            const Text('Choose\nRestaurant',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontFamily: 'font',
+                                fontSize: 10,
+                                fontWeight: FontWeight.bold
+                              ),
+                            ),
+
+                          ],
+                        ),
+
+
+                        Column(
+                          children: [
+                            Image.asset('images/payment.png', scale: 1.7,),
+                            SizedBox(height: size.height * 2.5/100,),
+                            const Text('Do the\npayment',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontFamily: 'font',
+                                fontSize: 10,
+                                fontWeight: FontWeight.bold
+                              ),
+                            ),
+                          ],
+                        ),
+
+
+                        Column(
+                          children: [
+                            Image.asset('images/enjoy.png', scale: 2,),
+                            SizedBox(height: size.height * 1/100,),
+                            const Text('On seat\nDelivery',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontFamily: 'font',
+                                fontSize: 10,
+                                fontWeight: FontWeight.bold
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-
-
-              SizedBox(height: size.height*1/100,),
-
-
-            ],
+            ),
           ),
         ),
-      ),
     );
   }
 }
